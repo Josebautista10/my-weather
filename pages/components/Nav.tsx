14045f6b50270a0d839b6019117aea4ebd7c1951
Josebautista10 <cooldj1020@gmail.com>
2022-04-29 14:39:20 -0600

    switch to tailwindCSS

---
+++ pages/components/Nav.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Nav() {
+  return (
+    <div >
+      My weather App
+    </div>
+  )
+}
+
+export default Nav
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import Form from './components/Form'
+import Nav from './components/Nav'
 
 const Home: NextPage = () => {
   
@@ -8,6 +9,7 @@ const Home: NextPage = () => {
   
   return (
     <div>
+      <Nav/>
         <Form/>
     </div>
   )
