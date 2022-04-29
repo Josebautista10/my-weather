7a121b5232558d807f8bc7f7582337da1ec4b478
Josebautista10 <cooldj1020@gmail.com>
2022-04-29 15:47:25 -0600

    built scaffolding of navbar and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
+import Footer from './components/Footer'
 import Form from './components/Form'
 import Nav from './components/Nav'
 
 const Home: NextPage = () => {
-  
-
-  
   return (
     <div>
-      <Nav/>
-        <Form/>
+      <Nav />
+      <Form />
+      <Footer />
     </div>
   )
 }
