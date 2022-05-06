d6c9b33736b4880e1e14eb40bfcca6c59f88b427
Josebautista10 <cooldj1020@gmail.com>
2022-05-06 13:56:54 -0600

    changed format of details

--- pages/components/Container.tsx
+++ pages/components/Container.tsx
@@ -25,39 +25,38 @@ function FormContainer() {
       })
       .catch((error) => setStatus(error.response.status))
   }
+
   console.log(data)
 
   return (
     <div className=' flex justify-center items-center bg-slate-100 h-screen/9'>
       <div className=' w-2/4 h-1/2 bg-yellow-400 rounded-3xl'>
-        {!loaded ? (
-          <div className='flex justify-center flex-col w-full justify-center h-3/4'>
-            <div className='flex justify-center flex-col'>
-              <label className='text-2xl ml-6'>Search for any city:</label>
-              <form
-                className=' h-1/2 justify-center flex-col align-center flex p-6'
-                onSubmit={handleSubmit}
-              >
-                <div className='flex '>
-                  <input
-                    required
-                    onChange={(e) => setSearchItem(e.target.value)}
-                    placeholder='My City'
-                    className='w-5/6 mr-3 p-1 rounded-md'
-                  />
-                  <button type='submit'>
-                    <FiSearch className='xs:text-lg  sm:text-xl  md:text-2xl lg:text-3xl text-white' />
-                  </button>
-                </div>
-              </form>
-            </div>
-            {status === 400 && <p>Error occurred please try again.</p>}
+        <div className='flex justify-center flex-col w-full justify-center h-1/2'>
+          <div className='flex justify-center flex-col'>
+            <label className='text-2xl ml-6'>Search for any city:</label>
+            <form
+              className=' h-1/2 justify-center flex-col align-center flex p-6'
+              onSubmit={handleSubmit}
+            >
+              <div className='flex '>
+                <input
+                  required
+                  onChange={(e) => setSearchItem(e.target.value)}
+                  placeholder='My City'
+                  className='w-5/6 mr-3 p-1 rounded-md'
+                />
+                <button type='submit'>
+                  <FiSearch className='xs:text-lg  sm:text-xl  md:text-2xl lg:text-3xl text-white' />
+                </button>
+              </div>
+            </form>
           </div>
-        ) : (
-          <div className='details'>
-            {loaded && <WeatherDetails details={data} />}
-          </div>
-        )}
+          {status === 400 && <p>Error occurred please try again.</p>}
+        </div>
+
+        <div className='details h-full'>
+          {loaded && <WeatherDetails details={data} className='h-full' />}
+        </div>
       </div>
     </div>
   )
--- pages/components/WeatherDetails.tsx
+++ pages/components/WeatherDetails.tsx
@@ -5,11 +5,11 @@ function WeatherDetails(props: any) {
   const { current, location } = props.details
   console.log(current)
   return (
-    <div className='flex flex-col'>
-      <div className='flex'>
+    <div className='flex flex-col items-center h-full'>
+      <div className='flex '>
         <h1></h1>
         <p>
-          {current.temp_c} {current.condition.text}
+          {current.temp_c}°C {current.condition.text}
           <img src={current.condition.icon} className=''></img>
         </p>
       </div>
