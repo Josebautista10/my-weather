977a575f471a455ca070afa5aa8445aad51d0e9d
Josebautista10 <cooldj1020@gmail.com>
2022-05-17 14:34:47 -0600

    added Tailwind elements for error toast

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,5 +33,8 @@ module.exports = {
   variants: {
     extend: {}
   },
-  plugins: []
+  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ]
 };
